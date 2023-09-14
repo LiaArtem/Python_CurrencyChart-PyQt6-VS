d3411a82a5225939f3d858fb442166a5a17b31f9
Artem Liakhovskyi <54625091+LiaArtem@users.noreply.github.com>
2023-09-14 14:13:33 +0300

    fix bat file

--- CurrencyChart_create_EXE_file.bat
+++ CurrencyChart_create_EXE_file.bat
@@ -5,6 +5,6 @@ rd /s /q build
 rd /s /q dist
 del /q main.spec
 
-pyinstaller -y --additional-hooks-dir=. --hidden-import ibm_db_sa.ibm_db --hidden-import ibm_db_dbi --hidden-import ibm_db --add-binary "%PYTHONPATH%\Lib\site-packages\ibm_db_dlls\ibm_db.dll;.\ibm_db_dlls" --add-data="%PYTHONPATH%\Lib\site-packages\clidriver;.\clidriver" --onefile --icon=icon.ico main.py
+pyinstaller --noconsole -y --additional-hooks-dir=. --hidden-import ibm_db_sa.ibm_db --hidden-import ibm_db_dbi --hidden-import ibm_db --add-binary "%PYTHONPATH%\Lib\site-packages\ibm_db_dlls\ibm_db.dll;.\ibm_db_dlls" --add-data="%PYTHONPATH%\Lib\site-packages\clidriver;.\clidriver" --onefile --icon=icon.ico main.py
 
 pause
