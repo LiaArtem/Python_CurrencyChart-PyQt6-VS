6e70a1efbf36b2bf2c8be8685b6a95bf35be14d7
Artem Liakhovskyi <artem.liakhovskyi@gmail.com>
2024-03-07 21:38:35 +0200

    update scripts and settings

--- CurrencyChart_create_EXE_file.bat
+++ CurrencyChart_create_EXE_file.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
-set PYTHONPATH="c:\Users\artem\AppData\Local\Programs\Python\Python312"
+set PYTHONPATH=c:\Users\artem\AppData\Local\Programs\Python\Python312
 
 rd /s /q build
 rd /s /q dist
 del /q main.spec
 
-pyinstaller --noconsole -y --additional-hooks-dir=. --onefile --icon=icon.ico main.py
+"%PYTHONPATH%\Scripts\pyinstaller.exe" --noconsole -y --additional-hooks-dir=. --onefile --icon=icon.ico main.py
 
 pause
